d0447bada9d500dee25b0b7f0574adb6fc497597
aymantabissi <aymanraisse7@gmail.com>
2026-05-01 19:33:25 +0200

    add env config

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-});
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+})
 
 api.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem('user'))
   if (user?.token) {
-    config.headers.Authorization = `Bearer ${user.token}`;
+    config.headers.Authorization = 'Bearer ' + user.token
   }
-  return config;
-});
+  return config
+})
 
-export default api;
+export default api
