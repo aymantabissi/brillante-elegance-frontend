3326d548d8768238df2e15d4ec62cf323b8230bb
aymantabissi <aymanraisse7@gmail.com>
2026-08-18 18:46:39 +0200

    web socket deploy

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = (import.meta.env.VITE_API_URL || 'http://localhost:5000/api').replace(/\/api\/?$/, '')
+const SOCKET_URL = new URL(import.meta.env.VITE_API_URL || 'http://localhost:5000/api').origin
 
 let socket = null
 
